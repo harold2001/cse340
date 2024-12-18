adf11d16de0a3bfbc00dd7d559b88a9f77e192ec
Harold Carazas <car23068@byui.edu>
2024-12-18 18:57:27 -0500

    feat: SQL code for final project enhancement added

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -258,3 +258,32 @@ UPDATE
 SET
     inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
     inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+
+-- Final Project Enhancement SQL Code
+-- Table: public.classification
+ALTER TABLE
+    public.classification
+ADD
+    COLUMN IF NOT EXISTS classification_approved boolean DEFAULT false,
+ADD
+    COLUMN IF NOT EXISTS account_id integer,
+ADD
+    COLUMN IF NOT EXISTS classification_approval_date timestamp DEFAULT CURRENT_TIMESTAMP,
+ADD
+    CONSTRAINT account_id FOREIGN KEY (account_id) REFERENCES public.account (account_id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION NOT VALID;
+
+CREATE INDEX IF NOT EXISTS fki_account_id ON public.classification USING btree (account_id ASC NULLS LAST) TABLESPACE pg_default;
+
+-- Table: public.inventory
+ALTER TABLE
+    public.inventory
+ADD
+    COLUMN IF NOT EXISTS inv_approved boolean DEFAULT false,
+ADD
+    COLUMN IF NOT EXISTS account_id integer,
+ADD
+    COLUMN IF NOT EXISTS inv_approved_date timestamp DEFAULT CURRENT_TIMESTAMP,
+ADD
+    CONSTRAINT account_id FOREIGN KEY (account_id) REFERENCES public.account (account_id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION NOT VALID;
+
+CREATE INDEX IF NOT EXISTS fki_account_id ON public.inventory USING btree (account_id ASC NULLS LAST) TABLESPACE pg_default;
